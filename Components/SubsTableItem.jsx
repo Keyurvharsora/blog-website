a82d6db7358c7f3ee630c64717c50ae768d69297
Keyurvharsora <keyurharsora09@gmail.com>
2024-07-07 13:00:48 +0530

    first commit

---
+++ Components/SubsTableItem.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+
+const SubsTableItem = ({ email,deleteEmail,mongoId,date, }) => {
+
+    const emailDate = new Date(parseInt(date));
+
+    return (
+        <tr className='bg-white border-b text-left'>
+            <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+                {email ? email : "No email given"}
+            </th>
+            <td className='px-5 py-4 hidden sm:block'>{emailDate.toLocaleDateString()}</td>
+            <td className='px-5 py-4 cursor-pointer' onClick={() => deleteEmail(mongoId)}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                </svg>
+            </td>
+        </tr>
+    )
+}
+
+export default SubsTableItem
